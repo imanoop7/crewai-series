9890370a96ecd3d64c350e734e324d1a826bd471
Anoop Maurya <64773373+imanoop7@users.noreply.github.com>
2025-12-29 21:16:56 +0530

    Replace custom tool class with DuckDuckGo search function
    Removed the MyCustomTool class and input schema, replacing them with a function-based tool using the @tool decorator. The new custom_search function performs a DuckDuckGo search and returns formatted results, simplifying the tool's implementation.

--- research_and_blog/src/research_and_blog/tools/custom_tool.py
+++ research_and_blog/src/research_and_blog/tools/custom_tool.py
@@ -1,19 +1,18 @@
-from crewai.tools import BaseTool
+from crewai.tools import BaseTool, tool
 from typing import Type
 from pydantic import BaseModel, Field
+from ddgs import DDGS
 
-
-class MyCustomToolInput(BaseModel):
-    """Input schema for MyCustomTool."""
-    argument: str = Field(..., description="Description of the argument.")
-
-class MyCustomTool(BaseTool):
-    name: str = "Name of my tool"
-    description: str = (
-        "Clear description for what this tool is useful for, you agent will need this information to use it."
+@tool
+def custom_search(query:str) -> str:
+    """Search the web using DuckDuckGo and return formatted results."""
+    results = DDGS().text("python programming", max_results=5)
+    results_str = "\n\n".join(
+    f"Title: {r['title']}\nURL: {r['href']}\nSummary: {r['body']}"
+    for r in results
     )
-    args_schema: Type[BaseModel] = MyCustomToolInput
+    print(results_str)
+    return results_str
 
-    def _run(self, argument: str) -> str:
-        # Implementation goes here
-        return "this is an example of a tool output, ignore it and move along."
+# custom_search()
+
